ea2abf724c78b104102811befb92fbb055adda1c
aipstudio <aipstudio@mail.ru>
2019-01-01 10:03:16 +0300

    9

--- mm.py
+++ mm.py
@@ -1,11 +1,14 @@
 import requests
 import configparser
+import xml.etree.ElementTree as ET
+import os
 from threading import Timer
 from datetime import datetime
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 m = []
+g = []
 pp=ss=ttmax=0
 ttmin=100
 def run():
@@ -35,6 +38,7 @@ def run():
         exit
     except requests.exceptions.ConnectTimeout:
         exit
+    os.system("powershell -NoProfile -ExecutionPolicy ByPass -file mm_gg.ps1")
     print (sh())
     if pp < 1600 or ss < 570 or ttmax > 73 or ttmin < 40:
         send_mail(sh())
@@ -66,7 +70,8 @@ def sh():
             qq+='<td>'+str(elem)+'</td>'
         q+='\n'
         qq+='</tr>'
-    qq+='</table></body></html>'
+    qq+='</table>'+get_ps_xml('192.168.1.2.xml')+get_ps_xml('192.168.1.3.xml')+get_ps_xml('192.168.1.7.xml')
+    qq+='</body></html>'
     f = open('index.html', 'w')
     f.write(qq)
     f.close()
@@ -90,5 +95,35 @@ def send_mail(q):
     text = msg.as_string()
     server.sendmail(email_login, email_to, text)
     server.quit()
+
+def get_ps_xml(q):
+    tree = ET.parse(q)
+    root = tree.getroot()
+    driver_version = root.find('driver_version')
+    r=''
+    rr='<table border=1 style="font-weight: bold;">'
+    for gpu in root.findall('gpu'):
+        product_name = gpu.find('product_name').text
+        fan_speed = gpu.find('fan_speed').text
+        for util in gpu.findall('utilization'):
+            gpu_util = util.find('gpu_util').text
+            memory_util = util.find('memory_util').text
+        for temp in gpu.findall('temperature'):
+            gpu_temp = temp.find('gpu_temp').text
+        for power in gpu.findall('power_readings'):
+            power_draw = power.find('power_draw').text
+        for clocks in gpu.findall('clocks'):
+            graphics_clock = clocks.find('graphics_clock').text
+            mem_clock = clocks.find('mem_clock').text
+            video_clock = clocks.find('video_clock').text
+        r+=product_name+' '+fan_speed+' '+gpu_util+' '+memory_util+' '+gpu_temp+' '
+        r+=power_draw+' '+graphics_clock+' '
+        r+=mem_clock+' '+video_clock+'\n'
+        rr+='<tr><td>'+product_name+'</td><td>'+fan_speed+'</td><td>'+gpu_util+'</td><td>'
+        rr+=memory_util+'</td><td>'+gpu_temp+'</td><td>'+power_draw+'</td><td>'
+        rr+=graphics_clock+'</td><td>'+mem_clock+'</td><td>'+video_clock+'</td></tr>'
+    rr+='</table>'
+    print (r)
+    return rr
 
 run()
