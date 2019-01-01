fa7d2113a961704516ad72aecde18cf99de76b80
aipstudio <aipstudio@mail.ru>
2019-01-01 16:12:05 +0300

    12

--- mm.py
+++ mm.py
@@ -5,7 +5,6 @@ from datetime import datetime
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 m=[]
-gg=''
 pp=ss=ttmax=0
 ttmin=100
 def run():
@@ -36,8 +35,27 @@ def run():
     except requests.exceptions.ConnectTimeout:
         exit
     os.system("powershell -NoProfile -ExecutionPolicy ByPass -file mm_gg.ps1")
-    gg='<table border=1 style="font-weight: bold;float:left"><tr><td>Name</td><td>Cooler</td><td>CPU</td><td>MEM</td><td>Temp</td><td>Watt</td><td>GPU Clock</td><td>MEM Clock</td><td>VIDEO Clock</td></tr>'+get_ps_xml('192.168.1.2.xml')+get_ps_xml('192.168.1.3.xml')+get_ps_xml('192.168.1.7.xml')+'</table>'
-    print (sh())
+
+    q='Power='+str(pp) + ' Sped='+str(ss) + ' Tmax='+str(ttmax) + ' Tmin='+str(ttmin) + '\n'
+
+    print (datetime.today())
+    print (q)
+
+    qq='<html><body style="background-color:#111111;color:#ffffff;font-weight:bold;">'
+    qq+='<p>'+str(datetime.today())+'</p><p>'+q+'</p>'
+
+    qq+='<table border=1 style="font-weight: bold;float:left;">'
+    qq+='<tr><td>Farm</td><td>Temp</td><td>Power</td><td>Hash</td><td>Ac</td><td>Rj</td></tr>'
+    qq+=get_array_json()+'</table>'
+    qq+='<table border=1 style="font-weight: bold;float:left"><tr><td>Name</td><td>Cooler</td><td>CPU</td><td>MEM</td><td>Temp</td><td>Watt</td><td>GPU Clock</td><td>MEM Clock</td><td>VIDEO Clock</td></tr>'
+    qq+=get_ps_xml('192.168.1.2.xml')+get_ps_xml('192.168.1.3.xml')+get_ps_xml('192.168.1.7.xml')+'</table>'
+
+    qq+='</body></html>'
+
+    f = open('index.html', 'w')
+    f.write(qq)
+    f.close()
+
     if pp < 1600 or ss < 570 or ttmax > 73 or ttmin < 40:
         send_mail(sh())
 
@@ -57,25 +75,17 @@ def add_array(j,r,n):
         ss+=s
         m.append([j,t,p,s,acs,rjs])
 
-def sh():
-    global gg
-    q='Power='+str(pp) + ' Sped='+str(ss) + ' Tmax='+str(ttmax) + ' Tmin='+str(ttmin) + '\n'
-    qq='<html><body style="background-color:#111111;color:#ffffff;font-weight:bold;"><p>'+str(datetime.today())+'</p><p>'+q+'</p><table border=1 style="font-weight: bold;float:left;">'
-    qq+='<tr><td>Farm</td><td>Temp</td><td>Power</td><td>Hash</td><td>Ac</td><td>Rj</td></tr>'
+def get_array_json():
+    rr=r=''
     for row in m:
-        qq+='<tr>'
+        rr+='<tr>'
         for elem in row:
-            q += str(elem) + ' '
-            qq+='<td>'+str(elem)+'</td>'
-        q+='\n'
-        qq+='</tr>'
-    qq+='</table>'+gg
-    qq+='</body></html>'
-    f = open('index.html', 'w')
-    f.write(qq)
-    f.close()
-    print (datetime.today())
-    return q
+            r += str(elem) + ' '
+            rr+='<td>'+str(elem)+'</td>'
+        r+='\n'
+        rr+='</tr>'
+    print(r)
+    return rr
 
 def send_mail(q):
     config = configparser.ConfigParser()
@@ -95,12 +105,11 @@ def send_mail(q):
     server.sendmail(email_login, email_to, text)
     server.quit()
 
-def get_ps_xml(q):
-    tree = ET.parse(q)
+def get_ps_xml(p1):
+    tree = ET.parse(p1)
     root = tree.getroot()
     driver_version = root.find('driver_version')
-    r=''
-    rr=''
+    rr=r=''
     for gpu in root.findall('gpu'):
         product_name = gpu.find('product_name').text
         fan_speed = gpu.find('fan_speed').text
