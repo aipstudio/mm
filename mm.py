9a7cd4fa5d2a1a0ac4a29785908c83c24cdad557
aipstudio <aipstudio@mail.ru>
2019-01-03 08:56:38 +0300

    1

--- mm.py
+++ mm.py
@@ -22,7 +22,7 @@ def run():
     temp_min=100
     claymore_table=xml_table=''
 
-    for x in ferma: #ewbf опрашиваем api удаленных майнеров через get
+    for x in ferma: #ewbf опрашиваем api удаленных майнеров через get 
         try:
             r = requests.get('http://'+x+':42000/getstat', timeout=(1))
             add_array (x,r,len(r.json()["result"]))
