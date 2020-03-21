4a3041a15021c0f03761ba054a083ffa0a8ff035
aipstudio <aipstudio@mail.ru>
2020-03-21 08:30:03 +0300

    1

--- test.py
+++
@@ -1,8 +0,0 @@
-import socket, json
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('192.168.1.7',3333))
-s.send('{"id":0,"jsonrpc":"2.0","method":"miner_getstat1"}'.encode("utf-8"))
-j=s.recv(2048)
-s.close()
-r=json.loads(j.decode("utf-8"))
-print (r)
