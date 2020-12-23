d8e53cf8c3dd1e7851aecf6acf0ed8beac6a5017
aipstudio <aipstudio@mail.ru>
2020-12-23 20:55:27 +0300

    1

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+WD=/root/mm
+cd $WD
+git clone https://github.com/aipstudio/mm.git $WD
+git pull
+
 docker build -t mm .
 docker rm mm --force
 docker run -d -p 8008:8008 --name mm mm
